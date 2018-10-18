2c0f9cc630de046490eaee7d8e0569b1b084fbc3
Fred Hornsey <fred@hornsey.us>
2018-10-17 21:58:07 -0500

    Reduce Logging

--- configure.sh
+++ configure.sh
@@ -3,3 +3,18 @@ cd OpenDDS
 ./configure --no-tests --target=android \
   --ace=${workspace}/ACE_TAO/ACE \
   --tao=${workspace}/ACE_TAO/TAO \
+
+pm=ACE_TAO/ACE/build/target/include/makeinclude/platform_macros.GNU
+function prepend_pm {
+  printf '%s\n%s' "$@" "$(cat $pm)" > $pm
+}
+
+prepend_pm "no_deprecated := 1"
+
+major_rev=$(echo $ndk | grep -oE '[0-9]+')
+minor_rev=$(echo $ndk | grep -oE '[a-j]' | tr '[a-j]' '[0-9]')
+if [ $major_rev -lt 16 ]
+then
+  prepend_pm "__NDK_MINOR__ := $minor_rev"
+  prepend_pm "__NDK_MAJOR__ := $major_rev"
+fi
--- get-NDK.sh
+++ get-NDK.sh
@@ -6,14 +6,6 @@ toolchain_name=$ndk-$arch-android-$api-toolchain
 echo $toolchain_name > toolchain
 
 wget https://dl.google.com/android/repository/$ndk_zip
-unzip $ndk_zip
+unzip -qq $ndk_zip
 
 ./$ndk_dir/build/tools/make_standalone_toolchain.py --arch $arch --api $api --install-dir $toolchain_name
-
-major_rev=$(echo $ndk | grep -oE '[0-9]+')
-minor_rev=$(echo $ndk | grep -oE '[a-j]' | tr '[a-j]' '[0-9]')
-if [ $major_rev -lt 16 ]
-then
-  pm=ACE_TAO/ACE/build/target/include/makeinclude/platform_macros.GNU
-  printf '%s\n%s\n%s\n' "__NDK_MAJOR__ = $major_rev" "__NDK_MINOR__ = $minor_rev" "$(cat $pm)" > $pm
-fi
