e9a6f486ceef42ce84de926b3e44747d54c5ad50
Fred Hornsey <fred@hornsey.us>
2018-10-17 21:29:35 -0500

    Fix setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
-export workspace="$(cd"$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd )"
+export workspace="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd )"
 export MPC_ROOT=$workspace/MPC
 export android_toolchain=${workspace}/$(cat ${workspace}/toolchain)
 export PATH=$android_toolchain/bin:${PATH}
