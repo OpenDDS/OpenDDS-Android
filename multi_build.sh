d000a729e9009954e522dcebbb624cbb32e3333f
Fred Hornsey <fred@hornsey.us>
2018-10-25 17:41:00 -0500

    Final Version of File Size Footprint Script

--- multi_build.sh
+++ multi_build.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
+set -e
 
 # Modified Version of Script Written by Timothy Simpson
-
-# Combinations to Test:
-# Not Inlined, Speed Optimized
-# Not Inlined, Size Optimized
-# Inlined, Speed Optimized
-# Not Inlined, Speed Optimized, Java
-# Not Inlined, Size Optimized, Java
-# Inlined, Speed Optimized, Java
-# Not Inlined, Speed Optimized, Security
-# Not Inlined, Size Optimized, Security
-# Inlined, Speed Optimized, Security
-# Not Inlined, Speed Optimized, Slim (Minimum Profile, No BITs)
-# Not Inlined, Size Optimized, Slim
-# Inlined, Speed Optimized, Slim
 
 source setenv.sh
 bash mk_toolchain.sh
@@ -24,16 +11,102 @@ security="--security --xerces3=${workspace}/xerces_build --openssl=${workspace}/
 java="--java=${workspace}/jdk"
 slim="--no-built-in-topics --no-content-subscription --no-ownership-profile --no-object-model-profile --no-persistence-profile"
 
-feature_options=(" " "$slim" "$security" "$java")
+feature_options=("$java" "$java $slim" "$java $security" "$java $slim $security")
 flag_options=("--no-inline --optimize" "--no-inline --macros=CCFLAGS+=-Os" "--optimize")
 
 target_dds="${workspace}/OpenDDS/build/target"
+target_ace_tao="${workspace}/ACE_TAO/ACE/build/target"
+ace_lib="$target_ace_tao/lib"
+dds_lib="$target_dds/lib"
+messenger="$target_dds/tests/DCPS/Messenger"
+java_messenger="$target_dds/java/tests/messenger"
 cpp_build_target="DDS_Messenger_Publisher"
-java_build_target="java_both_test"
-ace_lib_files="${workspace}/ACE_TAO/ACE/build/target/lib/*"
-dds_lib_files="${workspace}/OpenDDS/build/target/lib/*"
-cpp_output_files="${workspace}/OpenDDS/build/target/tests/DCPS/Messenger/publisher"
-java_output_files="${workspace}/OpenDDS/build/target/java/tests/messenger/both/classes/Both.class"
+java_build_target="idl2jni_compact java_both_test"
+
+ignore_libraries=(
+  libm.so
+  libdl.so
+  libc.so
+  libc++_shared.so
+  libcrypto.so
+  libcrypto.so.1.1
+  libssl.so
+  libssl.so.1.1
+  libxerces-c-3.2.so
+  libxerces-c.so
+)
+
+search_locations=(
+  "$dds_lib"
+  "$ace_lib"
+  "$messenger"
+)
+
+native_files=(
+  "$messenger/publisher"
+)
+
+java_files=(
+  "$java_messenger/both/classes/Both.class"
+  "$dds_lib/tao_java.jar"
+  "$dds_lib/i2jrt.jar"
+  "$dds_lib/OpenDDS_DCPS.jar"
+  "$dds_lib/i2jrt_compact.jar"
+)
+
+java_support_files=(
+  "$dds_lib/libidl2jni_runtime.so"
+  "$dds_lib/libOpenDDS_DCPS_Java.so"
+  "$dds_lib/libtao_java.so"
+)
+
+security_support_files=(
+  "$dds_lib/libOpenDDS_Security.so"
+)
+
+function findfile {
+  local path lib="$1"
+  shift
+  for path in $@
+  do
+    if [ -f $path/$lib ]
+    then
+      echo $path/$lib
+      return 0
+    fi
+  done
+  echo "Couldn't find $lib"
+  return 1
+}
+
+function contains {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+function get_deps {
+  local deps=()
+  for lib in $(ndk-depends $1 2>/dev/null)
+  do
+    if ! contains "$lib" "${ignore_libraries[@]}"
+    then
+      deps+=($lib)
+    fi
+  done
+  echo "${deps[@]}"
+}
+
+function sizeof {
+  if [ -f $1 ]
+  then
+    du -Lb $1 | cut -d $'\t' -f 1
+  else
+    echo "Error: $1 does not exist" 1>&2
+    echo "-1"
+  fi
+}
 
 count=0
 
@@ -43,9 +116,9 @@ mkdir -p ${report_dir}
 function configure_build_round {
   bash ${workspace}/clean_target.sh
   cd OpenDDS
-  ./configure ${configure_options}
+  ./configure ${configure_options} || exit 1
   cd build/target
-  make -j 8 $bt
+  make -j 8 $targets || exit 1
 }
 
 for features in "${feature_options[@]}"
@@ -61,34 +134,64 @@ do
     round_options="${features} ${flags}"
     configure_options="$common_configure_options $round_options"
 
-    bt="${cpp_build_target}"
-    lf="${ace_lib_files} ${dds_lib_files}"
-    of="${cpp_output_files} ${java_output_files}"
+    targets="${cpp_build_target}"
 
     echo $round_options > ${report_file}
-    echo "---" >> ${report_file}
 
     if [[ $features == *"--security"* ]]; then
-      bt="$bt OpenDDS_Security"
+      targets="$targets OpenDDS_Security"
     fi
 
     if [[ $features == *"--java"* ]]; then
-      bt="$bt $java_build_target"
+      targets="$targets $java_build_target"
     fi
 
     configure_build_round
+    cd ${workspace}
 
     if [ $? -eq 0 ]; then
-      ldd ${cpp_output_files} >> ${report_file}
-      echo "---" >> ${report_file}
-      # note, this will return empty for static libraries, but ideally it would return zero
-      # grab dependencies | limit to non-system libs (assumes lib directories not in path / no environment set) | parse out file name | find library file |
-      # get directory entry | make listing uniform | parse out size | combine into a single line with spaces | add +'s to line | feed into bc for sum
-      ldd ${cpp_output_files} | grep "not found" | cut -d ' ' -f 1 | xargs -I {} find . -name {} -type f | xargs -I {} ls -la {} | sed 's/ [ ]*/ /g' | cut -d ' ' -f 5 | xargs echo | sed 's/ / + /g' | bc >> ${report_file}
-      echo "---" >> ${report_file}
-      ls -latRL ${lf} ${of} >> ${report_file}
-      echo "---" >> ${report_file}
-      ls -1 ${lf} ${of} | xargs -I {} sh -c 'ls -laL {}; echo "---"; objdump -h {}; echo "---"' >> ${report_file}
+      round_native_files=("${native_files[@]}")
+      file_group=()
+
+      if [[ $features == *"--java"* ]]; then
+        round_native_files+=("${java_support_files[@]}")
+        file_group+=("${java_files[@]}")
+      fi
+
+      if [[ $features == *"--security"* ]]; then
+        round_native_files+=("${security_support_files[@]}")
+      fi
+
+      native_group_names=()
+      for file in "${round_native_files[@]}"
+      do
+        for dep in $(get_deps $file)
+        do
+          if ! contains "$dep" "${native_group_names[@]}"
+          then
+            native_group_names+=($dep)
+          fi
+        done
+      done
+
+      for file in "${native_group_names[@]}"
+      do
+        file_group+=($(findfile $file "${search_locations[@]}"))
+      done
+
+      total=0
+      for file in "${file_group[@]}"
+      do
+        file_size=$(sizeof $file)
+        if [ "$file_size" = "-1" ]
+        then
+          echo "$file is Missing!" >> ${report_file}
+          exit 1
+        fi
+        total=$(expr $total '+' $file_size)
+        printf '%s\t%s\n' $file $file_size >> ${report_file}
+      done
+      echo 'Total:' $total 'B' '(' $(expr $total '/' 1048576) 'MiB )' >> ${report_file}
     else
       echo "Build error!" >> ${report_file}
       echo "Build error!"
@@ -96,4 +199,3 @@ do
     fi
   done
 done
-
