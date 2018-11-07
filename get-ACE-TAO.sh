197a0f2b8d65b29dbf308a932c2cab382556c5e5
Johnny Willemsen <jwillemsen@remedy.nl>
2018-11-07 15:40:23 +0100

    Make use of DOC Group TAO and clone with a depth 1 to speedup git
        * get-ACE-TAO.sh:

--- get-ACE-TAO.sh
+++ get-ACE-TAO.sh
@@ -2,12 +2,10 @@ set -e
 
 if [ ! -d MPC ]
 then
-  git clone https://github.com/DOCGroup/MPC
+  git clone --depth 1 https://github.com/DOCGroup/MPC
 fi
 
 if [ ! -d ACE_TAO ]
 then
-  git clone https://github.com/iguessthislldo/ACE_TAO.git
-  cd ACE_TAO
-  git checkout igtd/android
+  git clone --depth 1 https://github.com/DOCGroup/ACE_TAO
 fi
