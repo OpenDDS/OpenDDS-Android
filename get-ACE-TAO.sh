bbb69e2954626111100212a8d96fc999f1eeedd5
Fred Hornsey <fred@hornsey.us>
2019-01-20 00:18:07 -0600

    Temporarily Use igtd/android

--- get-ACE-TAO.sh
+++ get-ACE-TAO.sh
@@ -7,5 +7,6 @@ fi
 
 if [ ! -d ACE_TAO ]
 then
-  git clone --depth 1 https://github.com/DOCGroup/ACE_TAO
+  #git clone --depth 1 https://github.com/DOCGroup/ACE_TAO
+  git clone --depth 1 https://github.com/iguessthislldo/ACE_TAO --branch 'igtd/android'
 fi
