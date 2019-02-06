8f3cb47a6930763fd9c3b93c127bfa79a0b90037
Fred Hornsey <fred@hornsey.us>
2019-02-06 11:01:10 -0600

    Don't use --sync-output on Travis

--- make.sh
+++ make.sh
@@ -1,13 +1,17 @@
+# Try to craft the optimal make command and export it as $make
 set -e
 
-# Try to craft the optimal make command
 make_command="make"
+
+# Use Number of Cores For Job Count, Default to 4
 core_count="$(grep -c ^processor /proc/cpuinfo)"
 if [ $? -ne 0 ]
 then
   core_count=4
 fi
 make="$make_command -j $core_count"
+
+# If not on Travis and Make version is at least 4, sync job output
 function make_version {
   $make_command --version | grep -Eo '[0-9]+\.[0-9]+' | head -n 1
 }
@@ -15,8 +19,12 @@ function make_version_cmp {
   expr $(make_version) "$1" "$2" > /dev/null
   return $?
 }
-if make_version_cmp '>=' 4
+if ! ${TRAVIS:-false}
 then
-  make="$make --output-sync"
+  if make_version_cmp '>=' 4
+  then
+    make="$make --output-sync"
+  fi
 fi
+
 export make
