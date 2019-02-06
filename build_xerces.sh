6d312eac3080a0b7bd02feff6ae8f7a815e3c0cb
Fred Hornsey <fred@hornsey.us>
2019-02-06 00:29:41 -0600

    Fix Iconv Not Being Built

--- build_xerces.sh
+++ build_xerces.sh
@@ -3,19 +3,19 @@ set -e
 source setenv.sh
 source make.sh
 
-# pushd iconv_source
-# ./configure \
-#   --prefix=$GNU_ICONV_ROOT\
-#   --host=$target \
-#   CC=${target}-${CC:-clang} \
-#   CXX=${target}-${CC:-clang++} \
-#   LD=${target}-${LD:-ld} \
-#   CFLAGS="-fPIE -fPIC" \
-#   LDFLAGS="-pie"
-# $make
-# mkdir -p $GNU_ICONV_ROOT
-# make install
-# popd
+pushd iconv_source
+./configure \
+  --prefix=$GNU_ICONV_ROOT\
+  --host=$target \
+  CC=${target}-${CC:-clang} \
+  CXX=${target}-${CC:-clang++} \
+  LD=${target}-${LD:-ld} \
+  CFLAGS="-fPIE -fPIC" \
+  LDFLAGS="-pie"
+$make
+mkdir -p $GNU_ICONV_ROOT
+make install
+popd
 
 pushd xerces_source
 cmake "-DCMAKE_INSTALL_PREFIX=$XERCESCROOT" "-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake" "-DANDROID_ABI=$abi" "-DANDROID_PLATFORM=android-$api" "-DANDROID_CPP_FEATURES=rtti exceptions"
