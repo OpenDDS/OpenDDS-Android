986fe172999c368b681f2647645c302d939e891f
Fred Hornsey <hornseyf@objectcomputing.com>
2020-12-29 19:02:26 -0600

    Print Settings in CI

---
+++ print_settings.sh
@@ -0,0 +1,11 @@
+# For for Quickly Reproducing Failing CI Builds
+source setenv.sh
+cat << EOF
+export arch=$arch
+export ndk=$ndk
+export api=$api
+export use_toolchain=$use_toolchain
+export ace_tao=$ace_tao
+export use_security=$use_security
+export use_java=$use_java
+EOF
