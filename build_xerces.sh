011707c6ec1097c09939d434e2b6a6c828a6ba04
Fred Hornsey <hornseyf@objectcomputing.com>
2021-01-04 14:09:05 -0600

    build_xerces.sh: Debug NDK Rev Check

--- build_xerces.sh
+++ build_xerces.sh
@@ -7,14 +7,19 @@ if [[ $api -lt 28 ]]
 then
   pushd iconv_source
   iconv_ld=${target}-ld
+  echo "ndk: $ndk"
+  echo "ndk_major_rev: $ndk_major_rev"
+  echo "ndk_minor_rev: $ndk_minor_rev"
   if [ $ndk_major_rev -ge 22 ]
   then
     # ${target}-ld doesn't exist in r22. GNU linker is still there under
     # different names, but we should use LLVM linker.
     iconv_ld=$android_toolchain/bin/ld.lld
   fi
+  echo "iconv_ld: $iconv_ld"
+  exit 1
   ./configure \
-    --prefix=$GNU_ICONV_ROOT\
+    --prefix=$GNU_ICONV_ROOT \
     --host=$target \
     CC=${target}-clang \
     CXX=${target}-clang++ \
