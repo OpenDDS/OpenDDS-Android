c98bcd3ce9cf8b59a1a4f0cfe1f9aa85d65832c4
Fred Hornsey <hornseyf@objectcomputing.com>
2021-08-26 15:22:50 -0500

    Test OpenDDS #2945
    https://github.com/objectcomputing/OpenDDS/pull/2945

--- override_settings.sh
+++ override_settings.sh
@@ -1,6 +1,6 @@
 # Branch from master and add settings here and push to repo to help test PRs
 
-# export OPENDDS_REPO=
-# export OPENDDS_BRANCH=
+export OPENDDS_REPO='https://github.com/iguessthislldo/OpenDDS'
+export OPENDDS_BRANCH='igtd/android-warnings'
 # export ACE_TAO_REPO=
 # export ACE_TAO_BRANCH=
