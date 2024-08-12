7e343992c1b9ac83fc4da0994e13240d8aaab18d
Fred Hornsey <hornseyf@objectcomputing.com>
2024-08-12 09:52:41 -0500

    Revert "Temporarily Override OpenDDS"
    This reverts commit 769c5103ee4e957d7f68aa4f4a3ab47f19516774.

--- override_settings.sh
+++ override_settings.sh
@@ -1,6 +1,6 @@
 # Branch from master and add settings here and push to repo to help test PRs
 
-export OPENDDS_REPO=https://github.com/iguessthislldo/OpenDDS
-export OPENDDS_BRANCH=igtd/cross-java
+# export OPENDDS_REPO=
+# export OPENDDS_BRANCH=
 # export ACE_TAO_REPO=
 # export ACE_TAO_BRANCH=
